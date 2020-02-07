0eb5f6c316e1bec2b05e15f88611a82b3da17b16
magusmachinae <edward.hughes1911@gmail.com>
2020-02-07 13:45:41 +0000

    sortable-table.student.cljs completed

--- src/building_re_frame_components/sortable_table/student.cljs
+++ src/building_re_frame_components/sortable_table/student.cljs
@@ -9,18 +9,71 @@
    ["Han Solo" "Blaster" "Good" 1.8]
    ["Obi-Wan Kenobi" "Light Saber" "Good" 1.82]
    ["Chewbacca" "Bowcaster" "Good" 2.28]
-   ["Darth Vader" "Light Saber" "Bad" 2.03]
-   ])
+   ["Darth Vader" "Light Saber" "Bad" 2.03]])
+
 
 (rf/reg-event-db
  :initialize
  (fn [db _]
-   {}))
+   {:tables {:new-hope {:header (first data)
+                        :rows (vec (rest data))}}}))
+
+(rf/reg-sub
+ :table
+ (fn [db [_ key]]
+   (get-in db [:tables key])))
+
+(defn sortable-table [table-key]
+  (let [s (reagent/atom {})]
+    (fn [table-key]
+      (let [table @(rf/subscribe [:table table-key])
+            key (:sort-key @s)
+            dir (:sort-direction @s)
+            rows (cond->>
+                  (:rows table)
+                  key (sort-by #(nth % key))
+                  (= :ascending dir) reverse)
+            sorts [key dir]]
+        [:div
+         [:table
+          {:style {:font-size "130%"}}
+          [:tr
+           (for [[i h] (map vector (range) (:header table))]
+             [:th {:style {:line-height ".75em"}
+                   :on-click #(cond
+                                (= [i :descending] sorts)
+                                (swap! s assoc
+                                       :sort-direction :ascending)
+                                (= [i :ascending] sorts)
+                                (swap! s dissoc
+                                       :sort-key :sort-direction)
+                                :else
+                                (swap! s assoc
+                                       :sort-key i
+                                       :sort-direction :descending))}
+
+              [:div {:style {:display :inline-block}}
+               h]
+              [:div {:style {:display :inline-block
+                             :vertical-align :middle
+                             :font-size "90%"}}
+               [:div {:style {:color (if (= [i :descending] sorts)
+                                       :black
+                                       "#aaa")}}
+                "▲"]
+               [:div {:style {:color (if (= [i :ascending] sorts)
+                                       :black
+                                       "#aaa")}}
+                "▼"]]])]
+          (for [row rows]
+            [:tr
+             (for [v row]
+               [:td v])])]]))))
 
 (defn ui []
   [:div
-   "Put table here"])
+   [sortable-table :new-hope]])
 
 (when-some [el (js/document.getElementById "sortable-table--student")]
-  (defonce _init (rf/dispatch-sync [:initialize]))
+  (defonce _init2 (rf/dispatch-sync [:initialize]))
   (reagent/render [ui] el))

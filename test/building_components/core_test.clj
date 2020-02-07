80b651b5e3ce9be2fdb3434d558cb80b8ac4b902
magusmachinae <edward.hughes1911@gmail.com>
2020-02-07 10:31:45 +0000

    Initial commit

---
+++ test/building_components/core_test.clj
@@ -0,0 +1,7 @@
+(ns building-components.core-test
+  (:require [clojure.test :refer :all]
+            [building-components.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
